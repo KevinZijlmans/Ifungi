1eae118b1db35f91692505a7f8e656823628ba9d
KevinZijlmans <kevin.zijlmans22@gmail.com>
2022-10-06 15:28:51 +0200

    added popups to marker

--- ifungi/src/components/Markers.js
+++ ifungi/src/components/Markers.js
@@ -1,12 +1,15 @@
 import React from "react"
-import { Marker } from 'react-leaflet'
-
+import { Marker, Popup } from 'react-leaflet'
 
 const Markers = props => {
   return (
     props.mushrooms.map((mushroom) => {
         return <Marker position={[mushroom.latlng[0], mushroom.latlng[1]]} >
-
+            <Popup  >
+            <h2>Name: {mushroom.name}</h2>
+            <p>Spots: {mushroom.spots}</p>
+            <p>Color: {mushroom.color}</p>
+        </Popup>
         </Marker>
     })
   )
